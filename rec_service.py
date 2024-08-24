588e71a8df45ff24c3ce2f435c281789cb7b7da0
Denis Chekarev <axinedy@inbox.ru>
2024-08-24 11:39:51 +0700

    test script introduced

--- rec_service.py
+++ rec_service.py
@@ -1,12 +1,14 @@
 import logging
+from urllib.request import Request
+
 from fastapi import FastAPI
+from starlette.responses import JSONResponse
+
 from recommendations import Recommendations
 from similar_items import SimilarItems
 
 
-features_store_url = "http://127.0.0.1:8010"
-events_store_url = "http://127.0.0.1:8020"
-
+MAX_K = 100
 log = logging.getLogger("uvicorn.error")
 
 rec_store = Recommendations()
@@ -31,6 +33,16 @@ sim_store.load(
 app = FastAPI(title="Сервис рекомендаций")
 
 
+class RangeError(Exception):
+    pass
+
+
+@app.exception_handler(RangeError)
+async def value_error_exception_handler(request: Request, exc: RangeError):
+    return JSONResponse(
+        status_code=400,
+        content={"message": str(exc)},)
+
 
 @app.get(
     "/recommendations",
@@ -40,6 +52,8 @@ app = FastAPI(title="Сервис рекомендаций")
         Если имеется онлайн-история, то, к полученным выше описанным способом рекомендациям, подмешиваются похожие трэки. 
         """)
 async def recommendations(user_id: int, k: int = 10):
+    if k > MAX_K or k <= 0:
+        raise RangeError("k is out of range")
     recs = rec_store.get(user_id, k)
     return {"recs": recs}
 
@@ -50,34 +64,6 @@ def dedup_ids(ids):
 
     return ids
 
-
-# @app.post("/recommendations_online")
-# async def recommendations_online(user_id: int, k: int = 100):
-#     """
-#     Возвращает список онлайн-рекомендаций длиной k для пользователя user_id
-#     """
-#
-#     headers = {"Content-type": "application/json", "Accept": "text/plain"}
-#
-#     # получаем последнее событие пользователя
-#     params = {"user_id": user_id, "k": 1}
-#     resp = requests.post(events_store_url + "/get", headers=headers, params=params)
-#     events = resp.json()
-#     events = events["events"]
-#     print(events)
-#
-#     # получаем список похожих объектов
-#     if len(events) > 0:
-#         item_id = events[0]
-#         params = {"item_id": item_id, "k": k}
-#         item_similar_items = requests.post(features_store_url + "/similar_items", headers=headers, params=params)
-#         item_similar_items = item_similar_items.json()
-#         print(item_similar_items)
-#         recs = item_similar_items  #item_similar_items[:k]
-#     else:
-#         recs = []
-#
-#     return {"recs": recs}
 
 @app.get("/")
 def root():
---
+++ test_service.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import sys
+from argparse import ArgumentParser
+import requests
+
+
+service_url = 'http://{}:{}'
+
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+
+def get_options():
+    parser = ArgumentParser(description='Recommendations service test script')
+    parser.add_argument('-c', '--cold', help='use cold user id', action='store_true', required=False)
+    parser.add_argument('-r', '--personal', help='use user id with personal recommendations', action='store_true', required=False)
+    parser.add_argument('-u', '--user', help='use specific user id', type=int, required=False)
+    parser.add_argument('-t', '--track', help='post specified track id to online history', type=int, required=False)
+    parser.add_argument('-k', '--count', help='quantity of recommendations', type=int, default=10)
+    parser.add_argument('-p', '--port', help='port', type=int, default=8000)
+    parser.add_argument('-i', '--ip',   help='ip address', type=str, default='127.0.0.1')
+    args = parser.parse_args()
+    cnt = sum([1 if a else 0 for a in [args.cold, args.personal, args.user]])
+    if 0 == cnt:
+        parser.print_help()
+        sys.exit(1)
+    elif 1 < cnt:
+        parser.print_help()
+        print('-c, -r and -u options are mutually exclusive')
+        sys.exit(2)
+    return args
+
+
+def get_recs(user_id: int, k: int):
+    print(f'Get recommendations for {user_id}, k={k}')
+    resp = requests.get(
+        service_url + "/recommendations", headers=headers, params={'user_id': user_id, "k": k})
+    if resp.status_code != 200:
+        print(f"status code: {resp.status_code}")
+    print(resp.json())
+
+
+if __name__ == '__main__':
+    options = get_options()
+    service_url = service_url.format(options.ip, options.port)
+    if options.cold:
+        user_id = 1
+    elif options.personal:
+        user_id = 778110
+    else:
+        user_id = options.user
+    print(options, user_id)
+
+    get_recs(user_id, options.count)
+
